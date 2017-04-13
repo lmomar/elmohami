5cdb427062d5249f4ebc6f80353ad42541a1e026
omar <omar@DESKTOP-O4EIU8J>
2017-04-13 17:56:38 +0100

    file parties pivot migration generated but not tested

---
+++ database/migrations/2017_04_13_165507_create_file_party_pivot_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class App\ extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('file_party', function (Blueprint $table) {
+            $table->integer('file_id')->unsigned()->index();
+            $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
+            $table->integer('party_id')->unsigned()->index();
+            $table->foreign('party_id')->references('id')->on('parties')->onDelete('cascade');
+            $table->primary(['file_id', 'party_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('file_party');
+    }
+}
